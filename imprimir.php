f80aa6f6a1b5ed90a08d9d8e7cc4b4418692b1e0
Hermes Aurelio Borges <hermesduff@yahoo.com.br>
2019-01-20 00:09:13 -0200

    mudei o primeiro echo e adicionei o ola udemy

---
+++ imprimir.php
@@ -0,0 +1,9 @@
+<?php
+	$num1 = 5;
+	$num2 = 10;
+	$res = $num1*$num2;
+
+	echo "A multiplicação de $num1 e $num2 é igual $res";
+
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-	echo "Ola MUndo!";
+	echo "Ola MUndo 456!";
+
+	echo "Ola Udemy";
 
 
 ?>
